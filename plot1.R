3973acf402e83571cbd263f71fd9a5bc0de41b24
Marcelo <marcelo.nardelli@gmail.com>
2018-11-09 00:42:17 -0200

    Specifying file dimensions for the plots

--- plot1.R
+++ plot1.R
@@ -6,6 +6,6 @@ if(!file.exists("household_power_consumption.txt")) {
 data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
 
-png(filename = "plot1.png")
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
 hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -6,7 +6,7 @@ if(!file.exists("household_power_consumption.txt")) {
 data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
 
-png(filename = "plot4.png")
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
 par(mfrow = c(2,2))
 data$DateAndTime <- strptime(paste(data$Date, data$Time, sep = " - "), format = "%d/%m/%Y - %H:%M:%S")
 Sys.setlocale("LC_ALL", "English")
